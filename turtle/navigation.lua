14cd0f7db5800cc3c3ac25ef33aaa84fb44d83ff
hypnnoo <alexrodriguez8231@gmail.com>
2026-01-03 23:25:01 -0500

    Update navigation.lua

--- turtle/navigation.lua
+++ turtle/navigation.lua
@@ -1,13 +1,18 @@
-local n={}
+-- turtle/navigation.lua
+-- Simple GPS-based vertical return helper (if you ever want it)
 
-function n.returnToSurface(_,y,_)
-    local _,cy=gps.locate()
-    while cy>y do
+local nav = {}
+
+function nav.returnToSurface(_, targetY, _)
+    local _, y = gps.locate()
+    if not y then return end
+
+    while y > targetY do
         if turtle.detectDown() then turtle.digDown() end
         turtle.down()
-        _,cy=gps.locate()
+        _, y = gps.locate()
     end
 end
 
-return n
+return nav
 

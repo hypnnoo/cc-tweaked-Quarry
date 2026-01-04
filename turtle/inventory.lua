98a659419d35a252062e2976a5333f475cc3cb69
hypnnoo <alexrodriguez8231@gmail.com>
2026-01-03 21:42:27 -0500

    Update inventory.lua

--- turtle/inventory.lua
+++ turtle/inventory.lua
@@ -1,24 +1,37 @@
 local i = {}
 
 local keep = {
-    ["minecraft:bucket"]=true
+    ["minecraft:bucket"] = true
 }
 
 function i.isFull(th)
-    th=th or 1
-    local e=0
-    for s=1,16 do
-        if turtle.getItemCount(s)==0 then e=e+1 end
+    th = th or 1
+    local empty = 0
+    for s = 1, 16 do
+        if turtle.getItemCount(s) == 0 then
+            empty = empty + 1
+        end
     end
-    return e<=th
+    return empty <= th
 end
 
+-- Dump items into a chest BEHIND the turtle
 function i.dumpToChest()
-    for s=1,16 do
+    -- turn around to face the chest behind
+    turtle.turnLeft()
+    turtle.turnLeft()
+
+    for s = 1, 16 do
         turtle.select(s)
-        local d=turtle.getItemDetail()
-        if d and not keep[d.name] then turtle.drop() end
+        local d = turtle.getItemDetail()
+        if d and not keep[d.name] then
+            turtle.drop()   -- drop into chest behind
+        end
     end
+
+    -- face back toward the quarry
+    turtle.turnLeft()
+    turtle.turnLeft()
     turtle.select(1)
 end
 

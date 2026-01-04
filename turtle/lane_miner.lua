60229c76518620949bca18deb1e8d4bfe12ca962
hypnnoo <alexrodriguez8231@gmail.com>
2026-01-03 20:44:59 -0500

    Update lane_miner.lua

--- turtle/lane_miner.lua
+++ turtle/lane_miner.lua
@@ -1,57 +1,125 @@
-local inv=require("inventory")
-local m={}
+local inventory = require("inventory")
 
-local function refuel()
-    if turtle.getFuelLevel()=="unlimited" then return end
-    if turtle.getFuelLevel()>500 then return end
-    for s=1,16 do
-        turtle.select(s)
-        local d=turtle.getItemDetail()
-        if d and d.name=="minecraft:lava_bucket" then
-            turtle.refuel(1)
+local lane_miner = {}
+
+-- Refuel using lava buckets if fuel is low
+local function ensureFuel()
+    local level = turtle.getFuelLevel()
+    if level == "unlimited" or level > 500 then return end
+
+    for slot = 1, 16 do
+        turtle.select(slot)
+        local detail = turtle.getItemDetail()
+        if detail and detail.name == "minecraft:lava_bucket" then
+            turtle.refuel(1) -- consumes lava, leaves empty bucket
             return
         end
     end
 end
 
-local function dig()
+local function digAround()
     if turtle.detect() then turtle.dig() end
     if turtle.detectUp() then turtle.digUp() end
     if turtle.detectDown() then turtle.digDown() end
 end
 
-local function fwd()
-    refuel() dig()
-    while not turtle.forward() do dig() sleep(0.1) end
-end
-
-local function down()
-    refuel() dig()
-    while not turtle.down() do dig() sleep(0.1) end
-end
-
-function m.mine(job,cb)
-    turtle.turnRight()
-    for i=1,job.xOffset do fwd() end
-    turtle.turnLeft()
-
-    for y=1,job.height do
-        local dir=true
-        for z=1,job.depth do
-            for x=1,job.width do
-                if inv.isFull() then return "FULL" end
-                dig()
-                if x<job.width then fwd() end
-            end
-            if z<job.depth then
-                if dir then turtle.turnRight() fwd() turtle.turnRight()
-                else turtle.turnLeft() fwd() turtle.turnLeft() end
-                dir=not dir
-            end
-        end
-        if cb then cb(math.floor((y/job.height)*100)) end
-        if y<job.height then down() end
+local function forward()
+    ensureFuel()
+    digAround()
+    while not turtle.forward() do
+        digAround()
+        sleep(0.1)
     end
 end
 
-return m
+local function down()
+    ensureFuel()
+    digAround()
+    while not turtle.down() do
+        digAround()
+        sleep(0.1)
+    end
+end
+
+local function mineCurrentCell()
+    digAround()
+    if inventory.isFull() then
+        return false
+    end
+    return true
+end
+
+function lane_miner.mineLane(job, statusCallback)
+    local width  = job.width   -- X size of this lane
+    local depth  = job.depth   -- Z size
+    local height = job.height or 10
+
+    -- 1) Move sideways along X to the start of this lane
+    -- Start assumption: turtle is facing +Z
+    turtle.turnRight() -- now facing +X
+    for i = 1, job.xOffset do
+        forward()
+    end
+    -- stay facing +X here for serpentine
+    local dir = 1 -- +X to start
+
+    local totalCells = width * depth * height
+    local minedCells = 0
+
+    for layer = 1, height do
+        for row = 1, depth do
+            -- mine starting cell of this row
+            if not mineCurrentCell() then
+                if statusCallback then
+                    statusCallback(math.floor((minedCells / totalCells) * 100))
+                end
+                return "FULL"
+            end
+            minedCells = minedCells + 1
+
+            -- traverse rest of row along X (width-1 moves)
+            for step = 1, width - 1 do
+                forward() -- along current X direction
+                if not mineCurrentCell() then
+                    if statusCallback then
+                        statusCallback(math.floor((minedCells / totalCells) * 100))
+                    end
+                    return "FULL"
+                end
+                minedCells = minedCells + 1
+            end
+
+            -- move to next row along Z (if any), flip X direction
+            if row < depth then
+                if dir == 1 then
+                    -- currently facing +X, go +Z, end facing -X
+                    turtle.turnLeft()
+                    forward()  -- +Z
+                    turtle.turnLeft()
+                    dir = -1
+                else
+                    -- currently facing -X, go +Z, end facing +X
+                    turtle.turnRight()
+                    forward()  -- +Z
+                    turtle.turnRight()
+                    dir = 1
+                end
+            end
+
+            if statusCallback then
+                statusCallback(math.floor((minedCells / totalCells) * 100))
+            end
+        end
+
+        -- go down one layer
+        if layer < height then
+            down()
+        end
+    end
+
+    if statusCallback then
+        statusCallback(100)
+    end
+end
+
+return lane_miner
